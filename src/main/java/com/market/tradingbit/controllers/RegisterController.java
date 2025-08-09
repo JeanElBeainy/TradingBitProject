253da06b1a1b4eedea2fb98d06aeff2584b806b6
jean <jeanelbeainy@gmail.com>
2025-08-09 14:31:52 +0300

    Add get mapping for RegisterController

--- src/main/java/com/market/tradingbit/controllers/RegisterController.java
+++ src/main/java/com/market/tradingbit/controllers/RegisterController.java
@@ -1,10 +1,15 @@
 package com.market.tradingbit.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/signup")
 public class RegisterController {
 
+    @GetMapping
+    public String register() {
+        return "signup";
+    }
 }
