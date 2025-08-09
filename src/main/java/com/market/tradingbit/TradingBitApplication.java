9ce756abcba461411259b11f2a4f48a225659621
jean <jeanelbeainy@gmail.com>
2025-08-09 13:33:14 +0300

    First initialization

---
+++ src/main/java/com/market/tradingbit/TradingBitApplication.java
@@ -0,0 +1,13 @@
+package com.market.tradingbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingBitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradingBitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/market/tradingbit/TradingBitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.tradingbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingBitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
