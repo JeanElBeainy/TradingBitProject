c1162ca75b95fd38c8836c778269faa25da43ab8
jean <jeanelbeainy@gmail.com>
2025-08-09 15:17:32 +0300

    Ad email annotation to registerDto

--- src/main/java/com/market/tradingbit/dtos/RegisterDto.java
+++ src/main/java/com/market/tradingbit/dtos/RegisterDto.java
@@ -1,5 +1,6 @@
 package com.market.tradingbit.dtos;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -11,6 +12,7 @@ public class RegisterDto {
     private String name;
 
     @NotEmpty(message = "Email is required")
+    @Email
     private String email;
 
     @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters")
