2ba99f572637e19965c95ed72ff8cef3ab48fd0a
jean <jeanelbeainy@gmail.com>
2025-08-09 15:26:39 +0300

    Use thymeleaf and Model for signup page

--- src/main/java/com/market/tradingbit/controllers/RegisterController.java
+++ src/main/java/com/market/tradingbit/controllers/RegisterController.java
@@ -1,6 +1,8 @@
 package com.market.tradingbit.controllers;
 
+import com.market.tradingbit.dtos.RegisterDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RegisterController {
 
     @GetMapping
-    public String register() {
+    public String register(Model model) {
+        model.addAttribute("registerDto", new RegisterDto());
         return "signup";
     }
 }
