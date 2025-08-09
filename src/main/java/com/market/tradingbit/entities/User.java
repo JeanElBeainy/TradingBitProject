f34c91e179819a0e7694a8359b68c56b7597edcf
jean <jeanelbeainy@gmail.com>
2025-08-09 15:06:17 +0300

    Create User entity

---
+++ src/main/java/com/market/tradingbit/entities/User.java
@@ -0,0 +1,35 @@
+package com.market.tradingbit.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
