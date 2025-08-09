b3f828d54cc7831665031b7a0b05f198583f31d1
jean <jeanelbeainy@gmail.com>
2025-08-09 14:31:12 +0300

    Create register controller

---
+++ src/main/java/com/market/tradingbit/controllers/RegisterController.java
@@ -0,0 +1,10 @@
+package com.market.tradingbit.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/signup")
+public class RegisterController {
+
+}
