0360d34dc45fef403a3720fbc9bb2ff89df71808
jean <jeanelbeainy@gmail.com>
2025-08-09 15:43:29 +0300

    Add post mapping without validation

--- src/main/java/com/market/tradingbit/controllers/RegisterController.java
+++ src/main/java/com/market/tradingbit/controllers/RegisterController.java
@@ -1,18 +1,49 @@
 package com.market.tradingbit.controllers;
 
 import com.market.tradingbit.dtos.RegisterDto;
+import com.market.tradingbit.entities.User;
+import com.market.tradingbit.repositories.UserRepository;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
+
 @Controller
+@AllArgsConstructor
 @RequestMapping("/signup")
 public class RegisterController {
+
+    private final UserRepository userRepository;
 
     @GetMapping
     public String register(Model model) {
         model.addAttribute("registerDto", new RegisterDto());
+        return "signup";
+    }
+
+    @PostMapping
+    public String register(@Valid @ModelAttribute RegisterDto registerDto) {
+
+        try {
+            User user = User.builder()
+                    .name(registerDto.getName())
+                    .email(registerDto.getEmail())
+                    .password(registerDto.getPassword())
+                    .createdAt(new Date())
+                    .build();
+            System.out.println(user.toString());
+            userRepository.save(user);
+        } catch (Exception e) {
+            System.out.println("Exception with POST register: " + e.getMessage());
+            return "signup";
+        }
         return "signup";
     }
 }
