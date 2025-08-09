4cbd763bdd6423f74aaa24a36b67cc5bda0f9a36
jean <jeanelbeainy@gmail.com>
2025-08-09 15:36:28 +0300

    Change minimum size of password to from 8 to 6 in RegisterDto

--- src/main/java/com/market/tradingbit/dtos/RegisterDto.java
+++ src/main/java/com/market/tradingbit/dtos/RegisterDto.java
@@ -15,7 +15,7 @@ public class RegisterDto {
     @Email
     private String email;
 
-    @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters")
+    @Size(min = 6, max = 50, message = "Password must be between 8 and 50 characters")
     private String password;
     private String confirmPassword;
 }
