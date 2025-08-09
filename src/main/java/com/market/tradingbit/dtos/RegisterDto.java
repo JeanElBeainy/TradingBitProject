fe2aadfe4f1bd3580ce583e217d8aadee204758a
jean <jeanelbeainy@gmail.com>
2025-08-09 15:14:46 +0300

    Create RegisterDto

---
+++ src/main/java/com/market/tradingbit/dtos/RegisterDto.java
@@ -0,0 +1,19 @@
+package com.market.tradingbit.dtos;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterDto {
+
+    @NotEmpty(message = "Name is required")
+    private String name;
+
+    @NotEmpty(message = "Email is required")
+    private String email;
+
+    @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters")
+    private String password;
+    private String confirmPassword;
+}
