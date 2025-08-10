dbb41cc368764d63bdf0136e2e82ad598353c677
jean <jeanelbeainy@gmail.com>
2025-08-10 16:43:55 +0300

    Remove model from signin mapping to implement default login functionality of spring

--- src/main/java/com/market/tradingbit/controllers/AuthController.java
+++ src/main/java/com/market/tradingbit/controllers/AuthController.java
@@ -11,8 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AuthController {
 
     @GetMapping
-    public String login(Model model) {
-        model.addAttribute("loginDto", new LoginDto());
+    public String login() {
         return "signin";
     }
 }
