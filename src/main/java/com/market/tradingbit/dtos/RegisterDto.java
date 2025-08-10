5380e4b369ed2910fac3f1a5ea38072646a95dbc
jean <jeanelbeainy@gmail.com>
2025-08-10 12:07:27 +0300

    Add conditions for RegisterDto fields

--- src/main/java/com/market/tradingbit/dtos/RegisterDto.java
+++ src/main/java/com/market/tradingbit/dtos/RegisterDto.java
@@ -9,13 +9,17 @@ import lombok.Data;
 public class RegisterDto {
 
     @NotEmpty(message = "Name is required")
+    @Size(min = 3, max = 30, message = "Username must be between 3 and 30 characters")
     private String name;
 
     @NotEmpty(message = "Email is required")
-    @Email
+    @Size(max = 255, message = "Email cannot exceed 255 characters")
+    @Email(message = "Invalid email address")
     private String email;
 
-    @Size(min = 6, max = 50, message = "Password must be between 8 and 50 characters")
+    @NotEmpty(message = "Password is required")
+    @Size(min = 6, max = 50, message = "Password must be between 6 and 50 characters")
     private String password;
+
     private String confirmPassword;
 }
