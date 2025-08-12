fc17de3b0af24b8123c241d7b12089d433781ebd
jean <jeanelbeainy@gmail.com>
2025-08-12 10:57:23 +0300

    Add model and principle in get mapping for dashboard

--- src/main/java/com/market/tradingbit/controllers/DashboardController.java
+++ src/main/java/com/market/tradingbit/controllers/DashboardController.java
@@ -1,15 +1,29 @@
 package com.market.tradingbit.controllers;
 
+import com.market.tradingbit.dtos.UserDashboardDto;
+import com.market.tradingbit.entities.User;
+import com.market.tradingbit.repositories.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Controller
 @RequestMapping("/dashboard")
+@AllArgsConstructor
 public class DashboardController {
 
+    private final UserRepository userRepository;
+
     @GetMapping
-    public String dashboard() {
+    public String dashboard(Model model, Principal principal) {
+        User user = userRepository.findByEmail(principal.getName());
+        UserDashboardDto userDashboardDto = new UserDashboardDto();
+        userDashboardDto.setName(user.getName());
+        model.addAttribute("userDashboardDto", userDashboardDto);
         return "dashboard";
     }
 }
