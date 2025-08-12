458c46986e374f6e784ef911cd207884044e6487
jean <jeanelbeainy@gmail.com>
2025-08-12 14:50:14 +0300

    Create CmcResponse class to list data of CryptoInfo

---
+++ src/main/java/com/market/tradingbit/models/CmcResponse.java
@@ -0,0 +1,12 @@
+package com.market.tradingbit.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+public class CmcResponse {
+    private List<CryptoInfo> data;
+}
