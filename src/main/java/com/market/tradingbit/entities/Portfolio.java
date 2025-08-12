7dd194e0d7a757d7c5fb455234ef880e9f668274
jean <jeanelbeainy@gmail.com>
2025-08-12 12:16:06 +0300

    Add portfolio entity class

---
+++ src/main/java/com/market/tradingbit/entities/Portfolio.java
@@ -0,0 +1,44 @@
+package com.market.tradingbit.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "portfolio")
+public class Portfolio {
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "crypto_balance")
+    private double cryptoBalance;
+
+    @Column(name = "stock_balance")
+    private double stockBalance;
+
+    @Column(name = "usd_balance")
+    private double usdBalance;
+
+    @Column(name = "total_balance")
+    private double totalBalance;
+
+    @Override
+    public String toString() {
+        return "Portfolio{" +
+                "id=" + id +
+                ", cryptoBalance=" + cryptoBalance +
+                ", stockBalance=" + stockBalance +
+                ", usdBalance=" + usdBalance +
+                ", totalBalance=" + totalBalance +
+                '}';
+    }
+}
