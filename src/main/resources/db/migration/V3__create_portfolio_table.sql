69d00b5656beb36c3e4639c81183ba67b743e519
jean <jeanelbeainy@gmail.com>
2025-08-12 12:15:12 +0300

    Create portfolio table in DB

---
+++ src/main/resources/db/migration/V3__create_portfolio_table.sql
@@ -0,0 +1,11 @@
+create table portfolio
+(
+    id             bigint auto_increment
+        primary key,
+    usd_balance    double not null,
+    crypto_balance double not null,
+    stock_balance  double not null,
+    total_balance  double not null,
+    constraint portfolio_users_id_fk
+        foreign key (id) references users (id)
+);
