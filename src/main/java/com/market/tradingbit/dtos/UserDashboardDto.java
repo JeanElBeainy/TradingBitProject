dc460e052ecfe1fadc699350770185aab4ddc13f
jean <jeanelbeainy@gmail.com>
2025-08-12 11:48:20 +0300

    Add stock balance in UserDashboardDto

--- src/main/java/com/market/tradingbit/dtos/UserDashboardDto.java
+++ src/main/java/com/market/tradingbit/dtos/UserDashboardDto.java
@@ -7,5 +7,6 @@ public class UserDashboardDto {
     private String name;
     private double usdBalance;
     private double cryptoBalance;
+    private double stockBalance;
     private double totalBalance;
 }
