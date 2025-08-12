79f0f35f8715ef0bfd3367c361719baf6aaf3a6b
jean <jeanelbeainy@gmail.com>
2025-08-12 10:57:01 +0300

    create user dashboard dto

---
+++ src/main/java/com/market/tradingbit/dtos/UserDashboardDto.java
@@ -0,0 +1,11 @@
+package com.market.tradingbit.dtos;
+
+import lombok.Data;
+
+@Data
+public class UserDashboardDto {
+    private String name;
+    private double usdBalance;
+    private double cryptoBalance;
+    private double totalBalance;
+}
