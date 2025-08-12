43b23482d33cdcf08faa8b356486d5841e99cd72
jean <jeanelbeainy@gmail.com>
2025-08-12 12:24:33 +0300

    Fix: change mapper to interface

---
+++ src/main/java/com/market/tradingbit/mappers/UserMapper.java
@@ -0,0 +1,10 @@
+package com.market.tradingbit.mappers;
+
+import com.market.tradingbit.dtos.RegisterDto;
+import com.market.tradingbit.entities.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+}
