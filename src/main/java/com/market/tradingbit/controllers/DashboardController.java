6510102d3715f03a172f80100de8f71de623e0cf
jean <jeanelbeainy@gmail.com>
2025-08-12 12:17:54 +0300

    Add method to get portfolio information from user to dashboard

--- src/main/java/com/market/tradingbit/controllers/DashboardController.java
+++ src/main/java/com/market/tradingbit/controllers/DashboardController.java
@@ -1,7 +1,9 @@
 package com.market.tradingbit.controllers;
 
 import com.market.tradingbit.dtos.UserDashboardDto;
+import com.market.tradingbit.entities.Portfolio;
 import com.market.tradingbit.entities.User;
+import com.market.tradingbit.repositories.PortfolioRepository;
 import com.market.tradingbit.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/dashboard")
@@ -17,6 +20,7 @@ import java.security.Principal;
 public class DashboardController {
 
     private final UserRepository userRepository;
+    private final PortfolioRepository portfolioRepository;
 
     @GetMapping
     public String dashboard(Model model, Principal principal) {
@@ -24,6 +28,12 @@ public class DashboardController {
         User user = userRepository.findByEmail(principal.getName());
         UserDashboardDto userDashboardDto = new UserDashboardDto();
         userDashboardDto.setName(user.getName());
+
+        System.out.println(user.getId());
+
+        Optional<Portfolio> portfolio = portfolioRepository.findById(user.getId());
+        System.out.println(portfolio);
+
         model.addAttribute("userDashboardDto", userDashboardDto);
         return "dashboard";
     }
