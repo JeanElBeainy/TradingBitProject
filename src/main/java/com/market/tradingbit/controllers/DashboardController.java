86aaa70835897294e6d4398d3d12772997c102e9
jean <jeanelbeainy@gmail.com>
2025-08-12 15:15:45 +0300

    Pass CoinMarketCapService to model

--- src/main/java/com/market/tradingbit/controllers/DashboardController.java
+++ src/main/java/com/market/tradingbit/controllers/DashboardController.java
@@ -5,6 +5,7 @@ import com.market.tradingbit.entities.Portfolio;
 import com.market.tradingbit.entities.User;
 import com.market.tradingbit.repositories.PortfolioRepository;
 import com.market.tradingbit.repositories.UserRepository;
+import com.market.tradingbit.services.CoinMarketCapService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,6 +23,7 @@ public class DashboardController {
 
     private final UserRepository userRepository;
     private final PortfolioRepository portfolioRepository;
+    private final CoinMarketCapService service;
 
     @GetMapping
     public String dashboard(Model model, Principal principal) {
@@ -40,6 +42,7 @@ public class DashboardController {
         userDashboardDto.setTotalBalance(portfolio.getTotalBalance());
 
         model.addAttribute("userDashboardDto", userDashboardDto);
+        model.addAttribute("cryptos", service.getLatestListings());
         return "dashboard";
     }
 }
