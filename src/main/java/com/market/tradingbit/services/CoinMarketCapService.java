d68fe5f1e36285cc94fdc9af72ae35e07784175d
jean <jeanelbeainy@gmail.com>
2025-08-12 14:54:01 +0300

    Create service class for fetching CoinMarketCap data from API

---
+++ src/main/java/com/market/tradingbit/services/CoinMarketCapService.java
@@ -0,0 +1,37 @@
+package com.market.tradingbit.services;
+
+import com.market.tradingbit.models.CmcResponse;
+import com.market.tradingbit.models.CryptoInfo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class CoinMarketCapService {
+    private final WebClient webClient;
+
+    public CoinMarketCapService(@Value("${coinmarketcap.api-keyy}") String apiKey) {
+        this.webClient = WebClient.builder()
+                .baseUrl("https://pro-api.coinmarketcap.com/v1")
+                .defaultHeader("X-CMC_PRO_API_KEY", apiKey)
+                .defaultHeader("Accept", "application/json")
+                .build();
+    }
+
+    public List<CryptoInfo> getLatestListings() {
+        CmcResponse response = webClient.get()
+                .uri("/cryptocurrency/listings/latest")
+                .retrieve()
+                .bodyToMono(CmcResponse.class)
+                .block();
+
+        if(response == null)
+            return Collections.emptyList();
+
+        return response.getData();
+    }
+
+}
