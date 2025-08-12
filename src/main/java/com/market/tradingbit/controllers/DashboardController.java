1b696d444ec11b1815de5273e12f87ecab3a4048
jean <jeanelbeainy@gmail.com>
2025-08-12 10:41:26 +0300

    Fix dashboard mapping

--- src/main/java/com/market/tradingbit/controllers/DashboardController.java
+++ src/main/java/com/market/tradingbit/controllers/DashboardController.java
@@ -2,8 +2,10 @@ package com.market.tradingbit.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/dashboard")
 public class DashboardController {
 
     @GetMapping
