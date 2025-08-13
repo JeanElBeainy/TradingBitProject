72c3e2f3f2ae4f9f2a81e4cc0bdf354ecd9c1db9
jean <jeanelbeainy@gmail.com>
2025-08-13 13:50:25 +0300

    Add SQL query in PortfolioRepository to get a list from user's id

--- src/main/java/com/market/tradingbit/repositories/PortfolioRepository.java
+++ src/main/java/com/market/tradingbit/repositories/PortfolioRepository.java
@@ -2,6 +2,13 @@ package com.market.tradingbit.repositories;
 
 import com.market.tradingbit.entities.Portfolio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
+
+    @Query("SELECT p FROM Portfolio p WHERE p.id = :id")
+    List<Portfolio> getPortfolioById(@Param("id") Long id);
 }
