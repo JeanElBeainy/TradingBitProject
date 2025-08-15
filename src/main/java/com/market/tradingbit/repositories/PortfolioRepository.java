0bfc11aca6815bb64dafd6a22196e9df576930aa
jean <jeanelbeainy@gmail.com>
2025-08-15 11:05:25 +0300

    Modify portfolio repository method

--- src/main/java/com/market/tradingbit/repositories/PortfolioRepository.java
+++ src/main/java/com/market/tradingbit/repositories/PortfolioRepository.java
@@ -1,6 +1,7 @@
 package com.market.tradingbit.repositories;
 
 import com.market.tradingbit.entities.Portfolio;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +10,8 @@ import java.util.List;
 
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
 
-    @Query("SELECT p FROM Portfolio p WHERE p.id = :id")
-    List<Portfolio> getPortfolioById(@Param("id") Long id);
+    //@EntityGraph(attributePaths = {"user_id"})
+    //@Query("SELECT p FROM Portfolio p WHERE p.userId = :user_id")
+    List<Portfolio> getPortfolioByUserId(Long userId);
+
 }
