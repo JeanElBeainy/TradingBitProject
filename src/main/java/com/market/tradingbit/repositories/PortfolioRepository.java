65a78a01c9c572e7190893e986e32f45a14d4d6c
jean <jeanelbeainy@gmail.com>
2025-08-15 11:31:59 +0300

    Implement method to get user portfolio by Id

--- src/main/java/com/market/tradingbit/repositories/PortfolioRepository.java
+++ src/main/java/com/market/tradingbit/repositories/PortfolioRepository.java
@@ -1,8 +1,10 @@
 package com.market.tradingbit.repositories;
 
 import com.market.tradingbit.entities.Portfolio;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,4 +16,6 @@ public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
     //@Query("SELECT p FROM Portfolio p WHERE p.userId = :user_id")
     List<Portfolio> getPortfolioByUserId(Long userId);
 
+    @Query("SELECT p.symbol FROM Portfolio p WHERE p.userId = :userId")
+    List<String> getPortfolioSymbolsByUserId(Long userId);
 }
