e2f507f4abc624c6ad540695e0f07d29df0103d8
jean <jeanelbeainy@gmail.com>
2025-08-15 14:26:19 +0300

    Create CryptoNameSymbol class

---
+++ src/main/java/com/market/tradingbit/models/CryptoNameSymbol.java
@@ -0,0 +1,13 @@
+package com.market.tradingbit.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CryptoNameSymbol {
+    private String name;
+    private String symbol;
+}
