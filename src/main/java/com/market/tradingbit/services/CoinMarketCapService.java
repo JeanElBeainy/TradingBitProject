3fb32973d7995c5c5182f5bda7c67d29a8b0898e
jean <jeanelbeainy@gmail.com>
2025-08-15 14:26:07 +0300

    Create method to get a list of Crypto name and symbol

--- src/main/java/com/market/tradingbit/services/CoinMarketCapService.java
+++ src/main/java/com/market/tradingbit/services/CoinMarketCapService.java
@@ -2,6 +2,7 @@ package com.market.tradingbit.services;
 
 import com.market.tradingbit.models.CmcResponse;
 import com.market.tradingbit.models.CryptoInfo;
+import com.market.tradingbit.models.CryptoNameSymbol;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -32,5 +33,12 @@ public class CoinMarketCapService {
 
         return response.getData();
     }
+
+    public List<CryptoNameSymbol> getLatestNameAndSymbol() {
+        return getLatestListings().stream()
+                .map(c -> new CryptoNameSymbol(c.getName(), c.getSymbol()))
+                .toList();
+    }
+
 
 }
