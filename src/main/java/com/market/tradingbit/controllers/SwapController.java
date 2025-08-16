37ee8e55bb559f34cc49d07c54c34d0d572914ea
jean <jeanelbeainy@gmail.com>
2025-08-16 20:40:06 +0300

    Add edge cases and implement full swap logic

--- src/main/java/com/market/tradingbit/controllers/SwapController.java
+++ src/main/java/com/market/tradingbit/controllers/SwapController.java
@@ -2,7 +2,9 @@ package com.market.tradingbit.controllers;
 
 import com.market.tradingbit.dtos.SwapDto;
 import com.market.tradingbit.entities.Portfolio;
+import com.market.tradingbit.entities.Type;
 import com.market.tradingbit.entities.User;
+import com.market.tradingbit.models.CryptoNamePrice;
 import com.market.tradingbit.models.CryptoNameSymbol;
 import com.market.tradingbit.repositories.PortfolioRepository;
 import com.market.tradingbit.repositories.UserRepository;
@@ -15,6 +17,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.sound.sampled.Port;
 import java.security.Principal;
 import java.util.List;
 
@@ -53,21 +56,47 @@ public class SwapController {
     @PostMapping("/crypto")
     public String cryptoSwap(Model model, @Valid @ModelAttribute("swap") SwapDto swap, Principal principal, BindingResult bindingResult) {
         if(principal == null) return "redirect:/login";
-        if(swap.getFrom().isBlank())
+        User user = userRepository.findByEmail(principal.getName());
+        if(swap.getFrom().isBlank()) {
             bindingResult.addError(new FieldError(
                     "swap", "from", "Please select a valid currency that you own."
             ));
-        if(swap.getTo().isBlank())
+        }
+        if(swap.getTo().isBlank()) {
             bindingResult.addError(new FieldError(
                     "swap", "to", "Please select a valid currency to swap."
             ));
-        User user = userRepository.findByEmail(principal.getName());
+        }
+        if(swap.getQuantity() <= 0) {
+            bindingResult.addError(new FieldError(
+                    "swap", "quantity", "Quantity cannot be less than or equal to zero."
+            ));
+        }
+            List<CryptoNamePrice> prices = service.getPricesBySymbols(swap.getFrom(), swap.getTo());
+        if(prices.size() < 2) {
+            bindingResult.addError(new FieldError(
+                    "swap", "quantity", "One or more of the currencies you selected are not valid."
+            ));
+        }
         if(bindingResult.hasErrors()) {
             populateModel(model, user);
             return "swap";
         }
+        System.out.println(prices.getFirst() + " " + prices.getLast());
+        float quantityPriceFrom = (float) (prices.getFirst().getPrice() * swap.getQuantity());
+        System.out.println("quantity price from: " + quantityPriceFrom);
+        float quantityPriceTo = (float) (quantityPriceFrom / prices.getLast().getPrice());
+        System.out.println("quantity price to: " + quantityPriceTo);
+        if(portfolioRepository.getItemBySymbolAndUserId(swap.getTo(), user.getId()) == null) {
+            portfolioRepository.save(Portfolio.builder()
+                            .symbol(swap.getTo())
+                            .purchaseType(Type.CRYPTO)
+                            .userId(user.getId())
+                            .name(prices.getLast().getName())
+                    .build());
+        }
         portfolioRepository.updatePortfolioQuantity(swap.getQuantity()*-1, swap.getFrom(), user.getId());
-        portfolioRepository.updatePortfolioQuantity(swap.getQuantity(), swap.getTo(), user.getId());
+        portfolioRepository.updatePortfolioQuantity(quantityPriceTo, swap.getTo(), user.getId());
         populateModel(model, user);
         return "swap";
     }
