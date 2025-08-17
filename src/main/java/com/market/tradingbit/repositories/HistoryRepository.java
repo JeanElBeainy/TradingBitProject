f21a75049e2cf46621bc78b64fc2ccb9a91b6032
jean <jeanelbeainy@gmail.com>
2025-08-17 20:26:23 +0300

    Fix history repository

--- src/main/java/com/market/tradingbit/repositories/HistoryRepository.java
+++ src/main/java/com/market/tradingbit/repositories/HistoryRepository.java
@@ -1,7 +1,8 @@
 package com.market.tradingbit.repositories;
 
+import com.market.tradingbit.entities.History;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HistoryRepository extends JpaRepository<> {
+public interface HistoryRepository extends JpaRepository<History, Long> {
 
 }
