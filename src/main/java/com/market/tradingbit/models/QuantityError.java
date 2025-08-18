d53077bf83150446c957dfb270cc0b95a231835f
jean <jeanelbeainy@gmail.com>
2025-08-18 09:22:47 +0300

    Create class QuantityError

---
+++ src/main/java/com/market/tradingbit/models/QuantityError.java
@@ -0,0 +1,17 @@
+package com.market.tradingbit.models;
+
+import com.market.tradingbit.dtos.SwapDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+
+@Getter
+@AllArgsConstructor
+public class QuantityError {
+    private Model model;
+    private Long userId;
+    private SwapDto swap;
+    private String message;
+    private BindingResult bindingResult;
+}
