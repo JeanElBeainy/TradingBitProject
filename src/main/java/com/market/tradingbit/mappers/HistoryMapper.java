743f38c153398aab2fd916b87d955670be4d6798
jean <jeanelbeainy@gmail.com>
2025-08-18 13:54:15 +0300

    Add toHistory method in HistoryMapper

--- src/main/java/com/market/tradingbit/mappers/HistoryMapper.java
+++ src/main/java/com/market/tradingbit/mappers/HistoryMapper.java
@@ -2,9 +2,11 @@ package com.market.tradingbit.mappers;
 
 import com.market.tradingbit.dtos.SuccessfulSwapDto;
 import com.market.tradingbit.entities.History;
+import com.market.tradingbit.models.FromUSD;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface HistoryMapper {
     History toHistory(SuccessfulSwapDto successfulSwapDto);
+    History toHistory(FromUSD fromUSD);
 }
