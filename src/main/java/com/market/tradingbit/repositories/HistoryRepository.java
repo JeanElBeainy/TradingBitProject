166a4148349ce23ad534fb11c2821e94093b5e0b
jean <jeanelbeainy@gmail.com>
2025-08-18 10:46:15 +0300

    Add query to get toQuantity in history

--- src/main/java/com/market/tradingbit/repositories/HistoryRepository.java
+++ src/main/java/com/market/tradingbit/repositories/HistoryRepository.java
@@ -2,7 +2,12 @@ package com.market.tradingbit.repositories;
 
 import com.market.tradingbit.entities.History;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
 
 public interface HistoryRepository extends JpaRepository<History, Long> {
 
+    @Query("SELECT h.toQuantity FROM History h WHERE h.id = :id")
+    BigDecimal getToQuantityById(Long id);
 }
