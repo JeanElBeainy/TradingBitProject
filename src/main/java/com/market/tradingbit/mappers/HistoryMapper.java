6e99ab5bdac034373a18c71156a557a8e95335a6
jean <jeanelbeainy@gmail.com>
2025-08-18 14:15:57 +0300

    Fix HistoryMapper parameters

--- src/main/java/com/market/tradingbit/mappers/HistoryMapper.java
+++ src/main/java/com/market/tradingbit/mappers/HistoryMapper.java
@@ -1,12 +1,12 @@
 package com.market.tradingbit.mappers;
 
+import com.market.tradingbit.dtos.HistoryDto;
 import com.market.tradingbit.dtos.SuccessfulSwapDto;
 import com.market.tradingbit.entities.History;
-import com.market.tradingbit.models.FromUSD;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface HistoryMapper {
     History toHistory(SuccessfulSwapDto successfulSwapDto);
-    History toHistory(FromUSD fromUSD);
+    History toHistory(HistoryDto historyDto);
 }
