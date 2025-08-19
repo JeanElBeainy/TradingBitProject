24e70be8e7d9a9a38a817c38d4ace4464c198c91
jean <jeanelbeainy@gmail.com>
2025-08-19 07:51:45 +0300

    Create SaveHistory class

---
+++ src/main/java/com/market/tradingbit/models/SaveHistory.java
@@ -0,0 +1,18 @@
+package com.market.tradingbit.models;
+
+import com.market.tradingbit.dtos.SwapDto;
+import com.market.tradingbit.entities.History;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+public class SaveHistory {
+    History history;
+    SwapDto swap;
+    Long userId;
+    BigDecimal volume;
+    BigDecimal exactSwapAmount;
+}
