cafe5590d2138b3713bf438418e3dfbd044063b6
jean <jeanelbeainy@gmail.com>
2025-08-19 10:42:01 +0300

    Add query to update user USD balance

--- src/main/java/com/market/tradingbit/repositories/BalanceRepository.java
+++ src/main/java/com/market/tradingbit/repositories/BalanceRepository.java
@@ -1,8 +1,19 @@
 package com.market.tradingbit.repositories;
 
 import com.market.tradingbit.entities.Balance;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
 
 
 public interface BalanceRepository extends JpaRepository<Balance, Long> {
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Balance b SET b.usdBalance = :amount WHERE b.id = :userId")
+    void updateUSDBalanceByAmountAndUserId(@Param("amount") BigDecimal amount, Long userId);
 }
