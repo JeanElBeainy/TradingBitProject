743fc6cd709070d68149db120112d91d795534c6
jean <jeanelbeainy@gmail.com>
2025-08-20 11:42:19 +0300

    Add model to profile controller

--- src/main/java/com/market/tradingbit/controllers/ProfileController.java
+++ src/main/java/com/market/tradingbit/controllers/ProfileController.java
@@ -1,16 +1,47 @@
 package com.market.tradingbit.controllers;
 
+import com.market.tradingbit.dtos.UserDashboardDto;
+import com.market.tradingbit.entities.Balance;
+import com.market.tradingbit.entities.User;
+import com.market.tradingbit.mappers.BalanceMapper;
+import com.market.tradingbit.repositories.BalanceRepository;
+import com.market.tradingbit.repositories.HistoryRepository;
+import com.market.tradingbit.repositories.PortfolioRepository;
+import com.market.tradingbit.repositories.UserRepository;
+import com.market.tradingbit.services.CoinMarketCapService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/profile")
 @AllArgsConstructor
 public class ProfileController {
+    private final UserRepository userRepository;
+    private final BalanceRepository balanceRepository;
+    private final PortfolioRepository portfolioRepository;
+    private final HistoryRepository historyRepository;
+    private final BalanceMapper balanceMapper;
+    private final CoinMarketCapService service;
 
-    @RequestMapping
-    public String profile() {
+    private void getUserInfo(Model model, User user) {
+        DashboardController.GetUserDashboard(model,
+                user,
+                service,
+                portfolioRepository,
+                balanceRepository,
+                balanceMapper
+        );
+    }
+
+    @GetMapping
+    public String profile(Principal principal, Model model) {
+        if(principal == null) return "redirect:/login";
+        User user = userRepository.findByEmail(principal.getName());
+        getUserInfo(model, user);
         return "profile";
     }
 }
