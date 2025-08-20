8bd495e4a29cd5d1ddaab7a22ffdabd9ee6d72f8
jean <jeanelbeainy@gmail.com>
2025-08-20 20:30:12 +0300

    Merge all .db files into one

--- src/main/resources/db/migration/V1__initial_migration.sql
+++ src/main/resources/db/migration/V1__initial_migration.sql
@@ -1,9 +1,48 @@
-create table users
+CREATE TABLE users
 (
-    id            BIGINT auto_increment
-        primary key,
-    name          VARCHAR(255) not null,
-    email         VARCHAR(255) not null,
-    password      VARCHAR(255) not null,
-    creation_date datetime     null
+    id            BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name          VARCHAR(30)  NOT NULL,
+    email         VARCHAR(255) NOT NULL,
+    password      VARCHAR(255) NOT NULL,
+    creation_date DATETIME     NULL,
+    role          VARCHAR(25)  NOT NULL
+);
+
+CREATE TABLE balance
+(
+    id             BIGINT PRIMARY KEY,
+    usd_balance    DECIMAL(30, 8) NOT NULL,
+    crypto_balance DECIMAL(30, 8) NOT NULL,
+    stock_balance  DECIMAL(30, 8) NOT NULL,
+    total_balance  DECIMAL(30, 8) NOT NULL,
+    total_volume   DECIMAL(30, 8) NOT NULL,
+    CONSTRAINT balance_users_id_fk
+        FOREIGN KEY (id) REFERENCES users (id)
+);
+
+CREATE TABLE portfolio
+(
+    id            BIGINT AUTO_INCREMENT PRIMARY KEY,
+    purchase_type VARCHAR(15)    NOT NULL,
+    symbol        VARCHAR(10)    NOT NULL,
+    quantity      DECIMAL(30, 8) NOT NULL,
+    user_id       BIGINT         NOT NULL,
+    name          VARCHAR(50)    NOT NULL
+);
+
+CREATE TABLE history
+(
+    id            BIGINT AUTO_INCREMENT PRIMARY KEY,
+    from_symbol   VARCHAR(10)    NOT NULL,
+    from_name     VARCHAR(50)    NOT NULL,
+    from_quantity DECIMAL(30, 8) NOT NULL,
+    from_price    DECIMAL(15, 8) NOT NULL,
+    to_symbol     VARCHAR(10)    NOT NULL,
+    to_name       VARCHAR(50)    NOT NULL,
+    to_quantity   DECIMAL(30, 8) NOT NULL,
+    to_price      DECIMAL(15, 8) NOT NULL,
+    user_id       BIGINT         NOT NULL,
+    volume        DECIMAL(15, 8) NOT NULL,
+    fee           DECIMAL(15, 8) NOT NULL,
+    time          VARCHAR(30)    NULL
 );
