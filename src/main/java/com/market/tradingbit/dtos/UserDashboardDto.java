d2b1e77a30eccb7e883b45d6501d67f7adec96b0
jean <jeanelbeainy@gmail.com>
2025-08-20 11:42:06 +0300

    Add role and totalVolume to UserDashboardDto

--- src/main/java/com/market/tradingbit/dtos/UserDashboardDto.java
+++ src/main/java/com/market/tradingbit/dtos/UserDashboardDto.java
@@ -1,12 +1,15 @@
 package com.market.tradingbit.dtos;
 
+import com.market.tradingbit.entities.Role;
 import lombok.Data;
 
 @Data
 public class UserDashboardDto {
     private String name;
+    private Role role;
     private String usdBalance;
     private String cryptoBalance;
     private String stockBalance;
     private String totalBalance;
+    private String totalVolume;
 }
