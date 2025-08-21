74e16d904f6990243a3656e87d70ba419cb5794d
jean <jeanelbeainy@gmail.com>
2025-08-21 19:00:55 +0300

    Fix: if user has no USD, it was re-assigned as type CRYPTO.

--- src/main/java/com/market/tradingbit/helpers/SaveToHistory.java
+++ src/main/java/com/market/tradingbit/helpers/SaveToHistory.java
@@ -5,6 +5,7 @@ import com.market.tradingbit.entities.Portfolio;
 import com.market.tradingbit.entities.Type;
 import com.market.tradingbit.models.AppendRepository;
 import com.market.tradingbit.models.SaveHistory;
+import com.market.tradingbit.repositories.BalanceRepository;
 import com.market.tradingbit.repositories.HistoryRepository;
 import com.market.tradingbit.repositories.PortfolioRepository;
 import lombok.AllArgsConstructor;
@@ -17,33 +18,44 @@ import java.time.format.DateTimeFormatter;
 @Component
 @AllArgsConstructor
 public class SaveToHistory {
-    private static final int CRYPTO_PRECISION = 8;
+    private static final int PRECISION = 8;
     private static final BigDecimal FEE_PERCENTAGE = new BigDecimal("0.0025");
-    private static final BigDecimal TOLERANCE = new BigDecimal("0.00000001");
     private final PortfolioRepository portfolioRepository;
     private final HistoryRepository historyRepository;
+    private final BalanceRepository balanceRepository;
 
     private BigDecimal getBigDecimalQuantity(History history, BigDecimal exactSwapAmount) {
         BigDecimal fromPrice = new BigDecimal(String.valueOf(history.getFromPrice()))
-                .setScale(CRYPTO_PRECISION, RoundingMode.HALF_EVEN);
+                .setScale(PRECISION, RoundingMode.HALF_EVEN);
         BigDecimal toPrice = new BigDecimal(String.valueOf(history.getToPrice()))
-                .setScale(CRYPTO_PRECISION, RoundingMode.HALF_EVEN);
+                .setScale(PRECISION, RoundingMode.HALF_EVEN);
 
         return fromPrice
                 .multiply(exactSwapAmount)
-                .divide(toPrice, CRYPTO_PRECISION, RoundingMode.HALF_EVEN)
-                .setScale(CRYPTO_PRECISION, RoundingMode.HALF_EVEN);
+                .divide(toPrice, PRECISION, RoundingMode.HALF_EVEN)
+                .setScale(PRECISION, RoundingMode.HALF_EVEN);
     }
 
-    private void appendToRepository(AppendRepository appendRepository) {
-        if(portfolioRepository.getItemBySymbolAndUserId(appendRepository.getSwap().getTo(), appendRepository.getUserId()) == null)
-            portfolioRepository.save(Portfolio.builder()
-                    .symbol(appendRepository.getSwap().getTo())
-                    .purchaseType(Type.CRYPTO)
-                    .userId(appendRepository.getUserId())
-                    .name(appendRepository.getToName())
-                    .quantity(appendRepository.getQuantityPriceTo())
-                    .build());
+    private void appendToRepository(AppendRepository appendRepository) { //4 queries
+        Portfolio portfolio = portfolioRepository.getItemBySymbolAndUserId(appendRepository.getSwap().getTo(), appendRepository.getUserId());
+        if(portfolio == null) {
+            if (appendRepository.getSwap().getTo().equals("US Dollar"))
+                portfolioRepository.save(Portfolio.builder()
+                        .symbol(appendRepository.getSwap().getTo())
+                        .purchaseType(Type.USD)
+                        .userId(appendRepository.getUserId())
+                        .name(appendRepository.getToName())
+                        .quantity(appendRepository.getQuantityPriceTo())
+                        .build());
+            else
+                portfolioRepository.save(Portfolio.builder()
+                        .symbol(appendRepository.getSwap().getTo())
+                        .purchaseType(Type.CRYPTO)
+                        .userId(appendRepository.getUserId())
+                        .name(appendRepository.getToName())
+                        .quantity(appendRepository.getQuantityPriceTo())
+                        .build());
+        }
         else
             portfolioRepository.updatePortfolioQuantity(appendRepository.getQuantityPriceTo(),
                     appendRepository.getSwap().getTo(),
@@ -53,16 +65,21 @@ public class SaveToHistory {
                 appendRepository.getSwap().getFrom(),
                 appendRepository.getUserId());
 
+        if(appendRepository.getSwap().getFrom().equals("US Dollar") || appendRepository.getSwap().getTo().equals("US Dollar"))
+            balanceRepository.updateUSDBalanceByAmountAndUserId(
+                    portfolioRepository.getQuantityBySymbolAndUserId("US Dollar", appendRepository.getUserId()),
+                    appendRepository.getUserId());
+
         portfolioRepository.deletePortfolioByQuantityIsLessThanEqualAndUserIdAndSymbol(BigDecimal.ZERO,
                 appendRepository.getUserId(),
                 appendRepository.getSwap().getFrom());
     }
 
-    public void saveHistory(SaveHistory saveHistory) {
+    public void saveHistory(SaveHistory saveHistory) { //1 query
         BigDecimal toQuantity = getBigDecimalQuantity(saveHistory.getHistory(), saveHistory.getExactSwapAmount());
         BigDecimal fee = toQuantity
                 .multiply(FEE_PERCENTAGE)
-                .setScale(CRYPTO_PRECISION, RoundingMode.HALF_EVEN);
+                .setScale(PRECISION, RoundingMode.HALF_EVEN);
 
         BigDecimal finalToQuantity = toQuantity.subtract(fee);
         saveHistory.getHistory().setToQuantity(finalToQuantity);
