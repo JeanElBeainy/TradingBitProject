54467ffd7054c186874e7e4749d74bcd75fbcded
jean <jeanelbeainy@gmail.com>
2025-08-22 13:11:49 +0300

    Extract register logic from RegisterController

--- src/main/java/com/market/tradingbit/controllers/RegisterController.java
+++ src/main/java/com/market/tradingbit/controllers/RegisterController.java
@@ -2,84 +2,33 @@ package com.market.tradingbit.controllers;
 
 import com.market.tradingbit.dtos.RegisterDto;
 import com.market.tradingbit.entities.*;
-import com.market.tradingbit.mappers.UserMapper;
-import com.market.tradingbit.repositories.BalanceRepository;
-import com.market.tradingbit.repositories.PortfolioRepository;
-import com.market.tradingbit.repositories.UserRepository;
+import com.market.tradingbit.helpers.registration.RegisterUser;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.math.BigDecimal;
-import java.util.Date;
 
 @Controller
 @AllArgsConstructor
 @RequestMapping("/signup")
 public class RegisterController {
 
-    private final UserRepository userRepository;
-    private final BalanceRepository balanceRepository;
-    private final UserMapper userMapper;
-    private final PortfolioRepository portfolioRepository;
+    private final RegisterUser register;
 
     @GetMapping
     public String register(Model model) {
         model.addAttribute("registerDto", new RegisterDto());
+        model.addAttribute("success", false);
         return "signup";
     }
 
     @PostMapping
-    public String register(@Valid @ModelAttribute RegisterDto registerDto, BindingResult bindingResult) {
-        if(!registerDto.getPassword().equals(registerDto.getConfirmPassword()))
-            bindingResult.addError(new FieldError("registerDto", "confirmPassword", "Passwords do not match"));
-
-        if(userRepository.findByEmail(registerDto.getEmail()) != null)
-            bindingResult.addError(new FieldError("registerDto", "email", "Email already exists"));
-
-        if(bindingResult.hasErrors())
-            return "signup";
-        try {
-            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-            User user = userMapper.toEntity(registerDto);
-            user.setPassword(encoder.encode(user.getPassword()));
-            user.setRole(Role.USER);
-            user.setCreatedAt(new Date());
-
-            User savedUser = userRepository.save(user);
-
-            Balance balance = Balance
-                    .builder()
-                    .id(savedUser.getId())
-                    .cryptoBalance(BigDecimal.valueOf(0))
-                    .stockBalance(BigDecimal.valueOf(0))
-                    .usdBalance(BigDecimal.valueOf(100_000))
-                    .totalBalance(BigDecimal.valueOf(100_000))
-                    .totalVolume(BigDecimal.valueOf(0))
-                    .build();
-            balanceRepository.save(balance);
-
-            portfolioRepository.save(Portfolio.builder()
-                            .purchaseType(Type.USD)
-                            .symbol("US Dollar")
-                            .quantity(BigDecimal.valueOf(100_000))
-                            .userId(savedUser.getId())
-                            .name("US Dollar Balance")
-                    .build());
-
-
-        } catch (Exception e) {
-            System.out.println("Exception with POST register: " + e.getMessage());
-            return "signup";
-        }
-        return "signup";
+    public String register(Model model, @Valid @ModelAttribute RegisterDto registerDto, BindingResult bindingResult) {
+        return register.registerUser(model, registerDto, bindingResult);
     }
 }
---
+++ src/main/java/com/market/tradingbit/helpers/registration/RegisterUser.java
@@ -0,0 +1,72 @@
+package com.market.tradingbit.helpers.registration;
+
+import com.market.tradingbit.dtos.RegisterDto;
+import com.market.tradingbit.entities.*;
+import com.market.tradingbit.mappers.UserMapper;
+import com.market.tradingbit.repositories.BalanceRepository;
+import com.market.tradingbit.repositories.PortfolioRepository;
+import com.market.tradingbit.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Component
+@AllArgsConstructor
+public class RegisterUser {
+
+    private final UserRepository userRepository;
+    private final BalanceRepository balanceRepository;
+    private final UserMapper userMapper;
+    private final PortfolioRepository portfolioRepository;
+
+    public String registerUser(Model model, RegisterDto registerDto, BindingResult bindingResult) {
+        if(!registerDto.getPassword().equals(registerDto.getConfirmPassword()))
+            bindingResult.addError(new FieldError("registerDto", "confirmPassword", "Passwords do not match"));
+
+        if(userRepository.findByEmail(registerDto.getEmail()) != null)
+            bindingResult.addError(new FieldError("registerDto", "email", "Email already exists"));
+
+        if(bindingResult.hasErrors())
+            return "signup";
+        try {
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            User user = userMapper.toEntity(registerDto);
+            user.setPassword(encoder.encode(user.getPassword()));
+            user.setRole(Role.USER);
+            user.setCreatedAt(new Date());
+
+            User savedUser = userRepository.save(user);
+
+            Balance balance = Balance
+                    .builder()
+                    .id(savedUser.getId())
+                    .cryptoBalance(BigDecimal.valueOf(0))
+                    .stockBalance(BigDecimal.valueOf(0))
+                    .usdBalance(BigDecimal.valueOf(100_000))
+                    .totalBalance(BigDecimal.valueOf(100_000))
+                    .totalVolume(BigDecimal.valueOf(0))
+                    .build();
+            balanceRepository.save(balance);
+
+            portfolioRepository.save(Portfolio.builder()
+                    .purchaseType(Type.USD)
+                    .symbol("US Dollar")
+                    .quantity(BigDecimal.valueOf(100_000))
+                    .userId(savedUser.getId())
+                    .name("US Dollar Balance")
+                    .build());
+
+
+        } catch (Exception e) {
+            System.out.println("Exception with POST register: " + e.getMessage());
+            return "signup";
+        }
+        model.addAttribute("success", true);
+        return "signup";
+    }
+}
