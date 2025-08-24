c597acdbe3c35dcaa1136752f961fcd3b4d8b7da
jean <jeanelbeainy@gmail.com>
2025-08-24 11:43:07 +0300

    Remove unused matcher

--- src/main/java/com/market/tradingbit/config/SecurityConfig.java
+++ src/main/java/com/market/tradingbit/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
                         ).permitAll()
 
                         //Access to users
-                        .requestMatchers("/crypto/**",
+                        .requestMatchers(
                                 "/profile/**",
                                 "/swap/**"
 
