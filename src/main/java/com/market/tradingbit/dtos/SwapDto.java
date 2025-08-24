ee8af105aa880c11277bc83f0ae1622440432161
jean <jeanelbeainy@gmail.com>
2025-08-24 20:22:19 +0300

    Add fromPrice and toPrice in SwapDto

--- src/main/java/com/market/tradingbit/dtos/SwapDto.java
+++ src/main/java/com/market/tradingbit/dtos/SwapDto.java
@@ -5,6 +5,8 @@ import lombok.Data;
 @Data
 public class SwapDto {
     private String from;
+    private double promisedFromPrice;
     private String to;
+    private double promisedToPrice;
     private String quantity;
 }
