cefd99865050f0abb49ba54a1e1b25f6d2916ade
jean <jeanelbeainy@gmail.com>
2025-08-25 10:36:49 +0300

    Implement UserDashboard in DashboardController

--- src/main/java/com/market/tradingbit/controllers/DashboardController.java
+++ src/main/java/com/market/tradingbit/controllers/DashboardController.java
@@ -1,10 +1,6 @@
 package com.market.tradingbit.controllers;
 
-import com.market.tradingbit.entities.User;
-import com.market.tradingbit.helpers.UpdateUserDetail;
-import com.market.tradingbit.models.CryptoInfo;
-import com.market.tradingbit.repositories.UserRepository;
-import com.market.tradingbit.services.CoinMarketCapService;
+import com.market.tradingbit.helpers.UserDashboard;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,34 +9,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.security.Principal;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
 @Controller
 @RequestMapping("/dashboard")
 @AllArgsConstructor
 public class DashboardController {
 
-    private final UserRepository userRepository;
-    private final CoinMarketCapService service;
-    private final UpdateUserDetail updateUserDetail;
+    private UserDashboard userDashboard;
 
     @GetMapping
     public String dashboard(Model model, Principal principal) {
-        User user = null;
-        if(principal != null)
-            user = userRepository.findByEmail(principal.getName());
-        if(user != null)
-            updateUserDetail.setUpUserDashboard(model, user);
-
-        model.addAttribute("cryptos", service.getLatestListings());
-        model.addAttribute("lastUpdated", new SimpleDateFormat("MMM dd, HH:mm:ss").format(new Date()));
+        userDashboard.getDashboard(model, principal);
         return "dashboard";
     }
 
     @GetMapping("/update-table")
     public String getCryptoTableFragment(Model model) {
-        List<CryptoInfo> cryptos = service.getLatestListings();
-        model.addAttribute("cryptos", cryptos);
+        userDashboard.getCryptoTable(model);
         return "dashboard :: crypto-table-body";
     }
 
@@ -52,8 +37,7 @@ public class DashboardController {
 
     @GetMapping("/update-user-info")
     public String updateUserInfo(Model model, Principal principal) {
-        if(principal != null)
-            updateUserDetail.setUpUserDashboard(model, userRepository.findByEmail(principal.getName()));
+        userDashboard.updateUserInfo(model, principal);
         return "dashboard :: update-user-info";
     }
 }
