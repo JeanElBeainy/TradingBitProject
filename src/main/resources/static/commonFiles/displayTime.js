acef5ddfafae0b1643f2cd8d5ea60b621dbe5b76
jean <jeanelbeainy@gmail.com>
2025-08-25 11:48:16 +0300

    Adjust time depending on user's timezone

---
+++ src/main/resources/static/commonFiles/displayTime.js
@@ -0,0 +1,10 @@
+const timestamp = 0;
+const userTime = new Date(timestamp).toLocaleString('en-US', {
+    month: 'short',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false
+});
+document.getElementById('lastUpdated').textContent = userTime;
